7aeebebe3d839b659e3afe619970ff7c39c075ee
Dat Bac Do <dat.b.do@gmail.com>
2018-06-08 20:53:43 -0400

    add graphql schema

--- src/graphql/resolvers/query/index.js
+++ src/graphql/resolvers/query/index.js
@@ -2,8 +2,17 @@ const helloResolver = async (rootValue, args, context, operations) => {
     return "world";
 };
 
+const fullNameResolver = async (rootValue, args, context, operations) => {
+    return {
+        first: "Dat",
+        middle: "B",
+        last: "Prytherch",
+    };
+};
+
 export default {
     Query: {
         hello: helloResolver,
+        fullName: fullNameResolver,
     },
 };
