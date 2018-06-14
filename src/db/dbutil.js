8aac62153d16403b3937fe8417ed97c599a922e6
Dat Bac Do <dat.b.do@gmail.com>
2018-06-14 11:10:53 -0400

    finish user schema

---
+++ src/db/dbutil.js
@@ -0,0 +1,12 @@
+export default {
+    variables: {
+        requiredString: {
+            type: String,
+            required: true,
+        },
+        requiredDate: {
+            type: Date,
+            required: true,
+        },
+    },
+};
--- src/dbinit.js
+++ src/dbinit.js
@@ -7,10 +7,7 @@ const main = async function() {
     try {
         await mongoose.connect(`${configStore.retrieve("/db/mongo/uri")}${configStore.retrieve("/db/mongo/dbName")}`);
         const connection = await mongoose.connection;
-        await connection.createCollection("test");
-        const collections = await connection.db.listCollections({}).toArray();
-        console.log(collections);
-        console.log("success");
+        await connection.dropDatabase();
         mongoose.connection.close();
     } catch (err) {
         console.log(err.message);
