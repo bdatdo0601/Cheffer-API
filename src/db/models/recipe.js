d29aca9737b60ddf0b933642c277f4c4aed26d02
Dat Bac Do <dat.b.do@gmail.com>
2018-07-24 21:53:40 -0400

    recipe db function is complete

--- src/db/models/recipe.js
+++ src/db/models/recipe.js
@@ -3,6 +3,7 @@ import MongoModels from "mongo-models";
 
 import RecipeType from "./recipeType";
 import FoodFilterParameter from "./foodFilterParameter";
+import Ingredient from "./ingredient";
 
 const recipeSchema = Joi.object({
     _id: Joi.object(),
@@ -41,14 +42,41 @@ class Recipe extends MongoModels {
         steps,
         associatedRecipes,
     }) {
+        const isRecipeExist = await this.findOne({ synonyms: name });
+        if (isRecipeExist) return isRecipeExist;
         const typeList = type.map(async item => {
-            const data = await RecipeType.createNewRecipeType({ name: item });
+            const data = await RecipeType.createNewRecipeType({
+                name: item,
+            });
             return data._id;
         });
         const specialitiesFilterList = specialitiesFilter.map(async item => {
             const data = await FoodFilterParameter.createNewFoodFilterParameter(item);
             return data._id;
         });
+        const ingredientList = ingredients.map(async ingredient => {
+            const data = await Ingredient.createNewIngredient(ingredient.ingredient);
+            const { amount, measurement } = ingredient;
+            return { ingredient: data._id, amount, measurement };
+        });
+        const documentInput = {
+            name,
+            synonyms: [...synonyms, name],
+            type: typeList,
+            specialitiesFilter: specialitiesFilterList,
+            headerImage,
+            ingredients: ingredientList,
+            steps,
+            associatedRecipes,
+        };
+        const document = new Recipe(documentInput);
+        const newRecipe = await this.insertOne(document);
+        return newRecipe[0];
+    }
+
+    static async getRecipes({ name }) {
+        const recipes = await this.find({ synonyms: name });
+        return recipes;
     }
 }
 
